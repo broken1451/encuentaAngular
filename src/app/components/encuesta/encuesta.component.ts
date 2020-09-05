2f1c0bd275ca01948dda443fcded1893ce5c4722
Adrian <adrianbravo145@gmail.com>
2020-09-05 19:37:49 -0400

    app graficabarra

--- src/app/components/encuesta/encuesta.component.ts
+++ src/app/components/encuesta/encuesta.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
+import { HttpClient } from '@angular/common/http';
+import { WebsocketService } from '../../services/websocket.service';
 
 @Component({
   selector: 'app-encuesta',
   templateUrl: './encuesta.component.html',
-  styleUrls: ['./encuesta.component.scss']
+  styleUrls: ['./encuesta.component.scss'],
 })
 export class EncuestaComponent implements OnInit {
+  public barChartOptions: ChartOptions = {
+    // scale: false,
+    responsive: true,
+  };
+  public barChartLabels: Label[] = [
+    'Pregunta 1',
+    'Pregunta 2',
+    'Pregunta 4',
+    'Pregunta 4',
+  ];
+  public barChartType: ChartType = 'bar';
 
-  constructor() { }
+  public barChartData: ChartDataSets[] = [
+    { data: [0, 0, 0, 0], label: 'Entrevistados' },
+  ];
+
+  constructor(
+    private httpClient: HttpClient,
+    public wsService: WebsocketService
+  ) {}
 
   ngOnInit(): void {
+    this.getData();
+    this.escucharSockets();
   }
 
+  getData() {
+    this.httpClient.get('http://localhost:5000/encuesta').subscribe((data: any) => {
+        console.log({ data });
+        this.barChartData = data.encuesta;
+        // this.lineChartData = data.grafica;
+    });
+  }
+
+  escucharSockets() {
+    this.wsService.listen('cambio-grafica').subscribe((data: any) => {
+      console.log('sockets: ', { data });
+      this.barChartData = data;
+    });
+  }
 }
